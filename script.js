e17e1706b0c093cb2e33b25ac58f8975692112bc
YashPanwar2003 <yashpanwar01102003@gmail.com>
2024-08-02 02:32:25 +0530

    final commit by Yash Panwar

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 import { quotes } from "./quotes.js";
 const button = document.querySelector("#button");
-document.onload = function () {
+window.onload = function () {
   const i = Math.floor(Math.random() * quotes.length);
   const obj = quotes[i];
+  document.querySelector("#quote").innerHTML = obj["quote"];
+  document.querySelector("#author").innerHTML = "- " + obj["author"];
 };
 button.addEventListener("click", function () {
   const electricity = document.querySelector("#electricbill");
@@ -18,17 +20,12 @@ button.addEventListener("click", function () {
   const alert2 = document.querySelector("#alert2");
   const calculator = document.querySelector(".calculator");
   const result = document.querySelector(".result");
-  const houseresult=document.querySelector("#houseresult")
-  const vehicleresult=document.querySelector("#vehicleresult")
-  const wasteresult=document.querySelector("#wasteresult")
-  const outcome=document.querySelector("#outcome")
-  const desc=document.querySelector("#desc")
-  const avgemission=1333333.33;
-  vehicle.defaultValue = 0;
-  flights.defaultValue = 0;
-  electricity.defaultValue = 0;
-  gas.defaultValue = 0;
-  fuel.defaultValue = 0;
+  const houseresult = document.querySelector("#houseresult");
+  const vehicleresult = document.querySelector("#vehicleresult");
+  const wasteresult = document.querySelector("#wasteresult");
+  const outcome = document.querySelector("#outcome");
+  const desc = document.querySelector("#desc");
+  const avgemission = 1333333.33;
 
   if (
     !(recyclepaperno.checked || recyclepaperyes.checked) ||
@@ -43,6 +40,12 @@ button.addEventListener("click", function () {
 
     return;
   }
+
+  vehicle.defaultValue = 0;
+  flights.defaultValue = 0;
+  electricity.defaultValue = 0;
+  gas.defaultValue = 0;
+  fuel.defaultValue = 0;
   var electricScore = "";
   var gasScore = "";
   var oilScore = "";
@@ -52,15 +55,15 @@ button.addEventListener("click", function () {
   var newspaperScore = "";
   var alumTinScore = "";
   var totalScore = "";
-  if (recyclepaperyes) {
-    newspaperScore = 184;
-  } else {
+  if (recyclepaperyes.checked) {
     newspaperScore = 0;
-  }
-  if (recycletinyes) {
-    alumTinScore = 166;
   } else {
+    newspaperScore = 184;
+  }
+  if (recycletinyes.checked) {
     alumTinScore = 0;
+  } else {
+    alumTinScore = 166;
   }
   electricScore = electricity.value * 105;
   gasScore = gas.value * 105;
@@ -71,14 +74,17 @@ button.addEventListener("click", function () {
   var travelScore = carScore + flightsScore;
   var wasteScore = newspaperScore + alumTinScore;
   totalScore = energyScore + travelScore + wasteScore;
-  let percentage=(Math.abs(totalScore-avgemission)/avgemission)*100;
-  percentage=Number.parseInt(percentage);
-  houseresult.innerHTML=`House & Domestic uses :<span> ${energyScore}</span> `
-  vehicleresult.innerHTML=`Travel & Vehicle uses : <span> ${travelScore}</span>`
-  wasteresult.innerHTML=`Recycling the Waste : <span>${wasteScore}</span>`
-  outcome.innerHTML=`Overall carbon Footprint : <span> ${totalScore}</span>`
-  desc.innerHTML=`Your Calculated Footprint is ${percentage}% ${"higher"?outcome>avgemission:"leseser"} than avg. Indian Footrprint`;
-
+  let percentage = (Math.abs(totalScore - avgemission) / avgemission) * 100;
+  percentage = Number.parseInt(percentage);
+  houseresult.innerHTML = `House & Domestic uses :<span> ${energyScore}</span> g/month`;
+  vehicleresult.innerHTML = `Travel & Vehicle uses : <span> ${travelScore}</span> g/month`;
+  wasteresult.innerHTML = `Recycling the Waste : <span>${wasteScore}</span> g/month`;
+  outcome.innerHTML = `Overall carbon Footprint : <span> ${totalScore}</span> g/month`;
+  desc.innerHTML = `Your Calculated Footprint is ${percentage}% ${
+    outcome > avgemission ? "higher" : "leseser"
+  } than avg. Indian Footrprint`;
+  calculator.classList.remove("fade-in");
+  result.classList.remove("fade-out");
   calculator.classList.add("fade-out");
   result.classList.add("fade-in");
   setTimeout(() => {
@@ -92,6 +98,15 @@ button2.addEventListener("click", () => {
 });
 const button3 = document.querySelector("#recalculate");
 button3.addEventListener("click", () => {
-   
-
+  const result = document.querySelector(".result");
+  const calculator = document.querySelector(".calculator");
+  result.classList.remove("fade-in");
+  calculator.classList.remove("fade-out");
+  result.classList.add("fade-out");
+  calculator.classList.add("fade-in");
+  setTimeout(() => {
+    result.style.display = "none";
+    calculator.style.display = "flex";
+  }, 600);
+  document.querySelector("#form").reset();
 });
